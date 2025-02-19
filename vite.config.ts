bdbd56a0184032fb830bdb5406f5dbdcab751b3c
Montejuado Michel <michel.montejuado@gmail.com>
2025-02-19 10:24:15 +0100

    feat(build): configure Rollup for manual chunking and Terser minification

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,29 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					"dexie": ["dexie", "dexie-react-hooks"],
+					"i18-next": ["i18next", "react-i18next", "i18next-http-backend", "i18next-browser-languagedetector"],
+					"react": ["react", "react-dom"],
+					"react-router": ["react-router"],
+					"recoil": ["recoil"],
+				}
+			}
+		},
+		minify: process.env.NODE_ENV === "production" ? "terser" : false,
+		terserOptions: {
+			compress: {
+				passes: 2
+			},
+			mangle: true,
+			format: {
+				comments: false
+			}
+		}
+	},
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
