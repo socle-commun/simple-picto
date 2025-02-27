21a0abd4d51c24ef9d0288e57fa09c7d5a9fed6b
Montejuado Michel <michel.montejuado@gmail.com>
2025-02-27 10:05:45 +0100

    refactor: enhance ResponsiveTopBar structure with semantic components and improve readability

--- src/components/nav/ResponsiveTopBar.tsx
+++ src/components/nav/ResponsiveTopBar.tsx
@@ -1,5 +1,11 @@
 import { type ReactNode } from "react";
 
+import Header from "@/lib/components/header";
+import Nav from "@/lib/components/nav";
+import Div from "@/lib/components/div";
+import Button from "@/lib/components/button";
+import Span from "@/lib/components/span";
+
 import { cn } from "@/utilities/cn";
 
 interface ResponsiveTopBarProps {
@@ -12,33 +18,33 @@ interface ResponsiveTopBarProps {
 export default function ResponsiveTopBar({ isOpen, onClick, brand, children }: ResponsiveTopBarProps) {
 	return (
 
-		<header>
-			<nav className={cn("w-full")}>
-				<div className={cn("flex flex-col md:items-center md:flex-row md:justify-between")}>
-					<div>
-						<div className={cn("flex items-center md:block")}>
+		<Header role="banner">
+			<Nav className={cn("w-full")}>
+				<Div className={cn("flex flex-col md:items-center md:flex-row md:justify-between")}>
+					<Div>
+						<Div className={cn("flex items-center md:block")}>
 							{brand}
 
-							<div className={cn("ml-auto md:hidden")}>
-								<button
+							<Div className={cn("ml-auto md:hidden")}>
+								<Button
 									className={cn("p-2 h-min rounded-full border border-gray-700 flex items-center justify-center")}
 									onClick={() => onClick()}
 								>
-									<span className={cn("sr-only")}>Toggle main menu</span>
-									<span aria-hidden="true" className={cn("icon")}>{isOpen ? "close" : "menu"}</span>
-								</button>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className={cn("flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0", isOpen ? "block" : "hidden")}>
+									<Span className={cn("sr-only")}>Toggle main menu</Span>
+									<Span aria-hidden="true" className={cn("icon")}>{isOpen ? "close" : "menu"}</Span>
+								</Button>
+							</Div>
+						</Div>
+					</Div>
+					<Div>
+						<Div className={cn("flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0", isOpen ? "block" : "hidden")}>
 							<ul className={cn("items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0")}>
 								{children}
 							</ul>
-						</div>
-					</div>
-				</div>
-			</nav>
-		</header>
+						</Div>
+					</Div>
+				</Div>
+			</Nav>
+		</Header>
 	);
 };
