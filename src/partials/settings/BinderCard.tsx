cd38b6f3a30585cec19cd755eb808fab33a1ffc0
Montejuado Michel <michel.montejuado@gmail.com>
2025-03-02 09:33:55 +0100

    feat: add BinderCard component and enhance settings page with binder management features

---
+++ src/partials/settings/BinderCard.tsx
@@ -0,0 +1,18 @@
+import { type ReactNode } from "react";
+
+import Div from "@/lib/components/div";
+
+import { cn } from "@/utilities/cn";
+
+export interface BinderCardProps {
+	children?: ReactNode;
+	className?: string;
+}
+
+export default function BinderCard({ children, className }: BinderCardProps) {
+	return (
+		<Div className={cn("flex flex-col px-4 pt-6 pb-2 gap-2 border border-zinc-600 dark:border-zinc-400 overflow-hidden rounded-md transition-all ease-in-out duration-150 hover:shadow-md", className)}>
+			{children}
+		</Div>
+	);
+}
--- src/partials/settings/SettingCard.tsx
+++ src/partials/settings/SettingCard.tsx
@@ -1,5 +1,7 @@
 import { type ReactNode } from "react";
 
+import Div from "@/lib/components/div";
+
 import { cn } from "@/utilities/cn";
 
 export interface SettingCardProps {
@@ -9,8 +11,8 @@ export interface SettingCardProps {
 
 export default function SettingCard({ children, className }: SettingCardProps) {
 	return (
-		<div className={cn("flex flex-col px-4 py-6 gap-2 bg-zinc-200 dark:bg-zinc-800 overflow-hidden rounded-lg shadow-md hover:shadow-lg transition-all ease-in-out duration-150", className)}>
+		<Div className={cn("flex flex-col px-4 py-6 gap-2 bg-zinc-200 dark:bg-zinc-800 overflow-hidden rounded-lg shadow-md hover:shadow-lg transition-all ease-in-out duration-150", className)}>
 			{children}
-		</div>
+		</Div>
 	);
 }
