72b4ed0ad5acc08208720ee4191478b0bd541469
Montejuado Michel <michel.montejuado@gmail.com>
2025-03-02 08:03:02 +0100

    feat: add binders label to settings and improve layout for better readability

--- src/partials/settings/ActiveBinderSelector.tsx
+++ src/partials/settings/ActiveBinderSelector.tsx
@@ -30,7 +30,8 @@ export default function ActiveBinderSelector() {
 
 	return (
 		<>
-			<label htmlFor="active-binder-selector" className={cn("text-2xl mb-4")}>{t("pages.settings.activeBinder")}</label>
+			<label htmlFor="active-binder-selector" className={cn("mb-2 text-2xl font-bold")}>{t("pages.settings.activeBinder")}</label>
+			<hr className={cn("border-zinc-400 dark:border-zinc-600 -mx-4")} />
 			<Select.Root
 				value={activeBinderUuid}
 				onValueChange={(uuid: string) => {
@@ -39,7 +40,7 @@ export default function ActiveBinderSelector() {
 						db.updateSetting(activeBinderUuidSetting);
 					}
 				}}>
-				<Select.Trigger id="active-binder-selector" className={cn("h-10 min-w-36 mt-auto mr-auto flex items-center justify-between px-2 gap-2 rounded-md border text-base select-none")}>
+				<Select.Trigger id="active-binder-selector" className={cn("h-10 min-w-36 mt-4 mr-auto flex items-center justify-between px-2 gap-2 rounded-md border text-base select-none")}>
 					<Select.Value placeholder={t("pages.settings.activeBinder")} />
 					<Select.Icon className={cn("icon")}>
 						keyboard_arrow_down
@@ -49,13 +50,13 @@ export default function ActiveBinderSelector() {
 					<Select.Positioner side="bottom" align="start">
 						<Select.Popup className={cn("py-1 rounded-md bg-zinc-200 dark:bg-zinc-800 shadow-lg outline transition-all")}>
 							{binders && binders.map((binder) => (
-								<Select.Item key={binder.uuid} value={binder.uuid} className={cn("grid min-w-full cursor-default grid-cols-[0.75rem_1fr] items-center gap-4 py-2 pr-4 pl-2.5 outline-none select-none hover:text-sky-50 hover:bg-sky-500")}>
+								<Select.Item key={binder.uuid} value={binder.uuid} className={cn("grid min-w-full cursor-default grid-cols-[0.75rem_1fr] items-center gap-8 py-2 pr-4 pl-2.5 outline-none select-none hover:text-sky-50 hover:bg-sky-500")}>
 									<Select.ItemIndicator className={cn("col-start-1 icon")}>
 										check
 									</Select.ItemIndicator>
-									<Select.ItemText className={cn("col-start-2 flex items-center gap-2")}>
+									<Select.ItemText className={cn("col-start-2 flex flex-col gap-2")}>
 										<span>{binder.title}</span>
-										<span> ({binder.description})</span>
+										<span className={cn("text-sm text-zinc-400 dark:text-zinc-600")}>{binder.description}</span>
 									</Select.ItemText>
 								</Select.Item>
 							))}
--- src/routes/settings/Page.tsx
+++ src/routes/settings/Page.tsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 
+import { useLiveQuery } from "dexie-react-hooks";
+
 import { useTranslation } from "react-i18next";
 
+import { db } from "@/features/persistence/db";
 import LocaleSelector from "@/features/i18n/LocaleSelector";
 import ColorModeToggle from "@/features/theming/ColorModeToggle";
 
@@ -13,6 +16,11 @@ import { cn } from "@/utilities/cn";
 
 export default function SettingsPage() {
 	const { t } = useTranslation();
+
+	const binders = useLiveQuery(
+		async () => db.getTranslatedBinders(),
+		[db, t]
+	);
 
 	useEffect(() => {
 		document.title = t("pages.titles.settings");
@@ -32,7 +40,13 @@ export default function SettingsPage() {
 					<ActiveBinderSelector />
 				</SettingCard>
 				<SettingCard>
-
+					<h2 className={cn("mb-2 text-2xl font-bold")}>{t("pages.settings.binders")}</h2>
+					<hr className={cn("border-zinc-400 dark:border-zinc-600 -mx-4")} />
+					<ul className={cn("mt-4 pl-8 list-disc")}>
+						{binders?.map((binder) => (
+							<li key={binder.uuid}>{binder.title}</li>
+						))}
+					</ul>
 				</SettingCard>
 			</div>
 		</>
