6c9f92e1ddbee1618549681e2aba51b48963206d
Montejuado Michel <michel.montejuado@gmail.com>
2025-03-03 13:42:04 +0100

    feat: improve accessibility by adding screen reader text for binder actions

--- src/partials/settings/BindersList.tsx
+++ src/partials/settings/BindersList.tsx
@@ -32,7 +32,7 @@ export default function BindersList() {
 				<h2 className={cn("text-2xl font-bold")}>{t("pages.settings.binders")}</h2>
 				<NavLink to="create" className={cn("ml-auto p-2 flex items-center justify-center gap-4 border-2 border-sky-500 text-sky-500 rounded-md cursor-pointer hover:scale-105 active:scale-95 transition-scale ease-in-out duration-150")}>
 					<Span className={cn("icon")}>add</Span>
-					{/* <Span>{t("pages.settings.new")}</Span> */}
+					<Span className={cn("sr-only")}>{t("pages.settings.new")}</Span>
 				</NavLink>
 			</Div>
 			<hr className={cn("border-zinc-400 dark:border-zinc-600 -mx-4")} />
@@ -48,13 +48,13 @@ export default function BindersList() {
 						<Div className={cn("flex justify-end gap-2 -mx-4 px-2")}>
 							<NavLink to={binder.uuid} className={cn("flex items-center justify-center gap-2 p-2 text-sm md:text-base bg-sky-500 text-sky-50 rounded-md cursor-pointer hover:scale-105 active:scale-95 transition-scale ease-in-out duration-150")}>
 								<Span className={cn("icon")}>edit</Span>
-								{/* <Span>{t("pages.settings.edit")}</Span> */}
+								<Span className={cn("sr-only")}>{t("pages.settings.edit")}</Span>
 							</NavLink>
 							<Button
 								onClick={() => onDeleteBinder(binder.uuid)}
 								className={cn("flex items-center justify-center gap-2 p-2 text-sm md:text-base border-2 border-red-500 text-red-500 rounded-md cursor-pointer hover:scale-105 active:scale-95 transition-scale ease-in-out duration-150")}>
 								<Span className={cn("icon")}>delete</Span>
-								{/* <Span>{t("pages.settings.delete")}</Span> */}
+								<Span className={cn("sr-only")}>{t("pages.settings.delete")}</Span>
 							</Button>
 						</Div>
 					</BinderCard>
