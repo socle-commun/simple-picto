648ecc00c3c6dd392937c607ad1e49fc3dfb51a2
Montejuado Michel <michel.montejuado@gmail.com>
2025-03-03 11:08:17 +0100

    refactor: better responsiveness

--- src/routes/settings/Layout.tsx
+++ src/routes/settings/Layout.tsx
@@ -7,12 +7,10 @@ import { cn } from "@/utilities/cn";
 
 export default function SettingsLayout() {
 	return (
-		<>
-			<Main role="main" className={cn("p-2 sm:p-4 lg:p-8")}>
-				<Div className={cn("flex flex-col gap-4 lg:w-3xl lg:mx-auto ")}>
-					<Outlet />
-				</Div>
-			</Main>
-		</>
+		<Main role="main" className={cn("p-2 sm:p-4 lg:p-8")}>
+			<Div className={cn("flex flex-col gap-4 lg:w-3xl lg:mx-auto ")}>
+				<Outlet />
+			</Div>
+		</Main>
 	)
 }
